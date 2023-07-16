db466baa609c3295c24cfb4106fe94ec2e09eeb6
Anvesha Dixit <91267841+anveshadixit@users.noreply.github.com>
2023-07-16 20:32:11 +0530

    Time: 3 ms (79.43%), Space: 11 MB (92.26%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+      
+        for(int i=0;i<n;i++)
+        {
+            if(target==nums[i])
+            {
+                return i;
+            }
+    
+        }
+        return -1;
+    }
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+//  int beg=0,end=nums.size()-1,mid;
+//         while(beg<=end)
+//         {
+//             mid=(beg+end)/2;
+//             if(nums[mid]==target)
+//                 return mid;
+//             if(nums[beg]<=nums[mid])
+//             {
+//                 if(target<=nums[mid] && target>=nums[beg])
+//                     end=mid-1;
+//                 else
+//                     beg=mid+1;
+//             }
+            
+//             else
+//             {
+//                 if(target>=nums[mid] && target<=nums[end])
+//                    beg=mid+1;
+//                 else
+//                     end=mid-1;
+//             }
+            
+//         }
+//         return -1;
+//     }
