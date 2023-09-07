18a257cad9f57a9ef1cbfc874f48cdcd972fa4be
Anvesha Dixit <91267841+anveshadixit@users.noreply.github.com>
2023-09-07 22:22:31 +0530

    Added solution - LeetHub

---
+++ Common Divisor - GFG/common-divisor.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int common_divisor(int a, int b);
+
+int main() {
+    
+    int t;cin>>t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        cout << common_divisor(a, b) << endl;
+        
+    }
+    
+	return 0;
+}
+// } Driver Code Ends
+
+
+int common_divisor(int a, int b)
+{
+    // Complete the function
+     int count=0;
+
+    for(int i=1;i<=b;i++)
+
+    {
+
+        if(a%i==0 && b%i==0)
+
+        {
+
+            count++;
+
+        }
+
+    }
+
+    return count;
+    
+}
